c85ce04f33a936444b7aa52e573ec2b8a666d0fd
Austin Pauley <austin2118ace@gmail.com>
2025-05-20 11:33:47 -0400

    New sliding window count-based frequency calculation

--- sniffing/helpers/frequency.py
+++ sniffing/helpers/frequency.py
@@ -24,23 +24,48 @@ def calc_frequencies(true_inhales, true_exhales):
     return inhale_frequencies, exhale_frequencies, inhale_times, exhale_times
 
 
-def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, window_size: int=100, window_hop: int=50):
+def _calc_freq(counts, bin_dur_ms):
+    if counts == 0:
+        return 0
+    else:
+        return 1 / (counts / (bin_dur_ms / 1000))
+
+
+def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, window_size_ms: int=100, window_hop_ms: int=50):
     """
 
     :param inhale_ts:
     :param trial_timestamps:
-    :param window_size:
-    :param window_hop:
+    :param window_size_ms:
+    :param window_hop_ms:
     :return:
     """
-    ts_bins = pd.DataFrame()
+    pre_fv_ts_bins = pd.DataFrame(columns=['count', 'frequency'])
+    post_fv_ts_bins = pd.DataFrame(columns=['count', 'frequency'])
 
-    pre_fv_timestamps = trial_timestamps.loc[:0]
-    post_fv_timestamps = trial_timestamps.loc[0:]
+    pre_fv_timestamps = trial_timestamps[trial_timestamps <= 0]
+    post_fv_timestamps = trial_timestamps[trial_timestamps >= 0]
 
-    num_pre_fv_bins = (len(pre_fv_timestamps) - window_size) // window_hop
-    pre_fv_bin_starts = np.linspace(pre_fv_timestamps[0], (pre_fv_timestamps[-1] - window_size), num_pre_fv_bins)
+    win_end = pre_fv_timestamps[-1]
+    win_start = win_end - window_size_ms
 
-    num_post_fv_bins = (len(post_fv_timestamps) - window_size) // window_hop
-    post_fv_bin_starts = np.linspace(post_fv_timestamps[0], (post_fv_timestamps[-1] - window_size), num_post_fv_bins)
+    while win_start >= pre_fv_timestamps[0]:
+        num_current_bin = inhale_ts.between(win_start, win_end).sum()
+        frequency = _calc_freq(num_current_bin, window_size_ms)
+        pre_fv_ts_bins.loc[win_end, 'count'] = num_current_bin
+        pre_fv_ts_bins.loc[win_end, 'frequency'] = frequency
+        win_end -= window_hop_ms
+        win_start = win_end - window_size_ms
 
+    win_start = post_fv_timestamps[0]
+    win_end = win_start + window_size_ms
+
+    while win_end <= post_fv_timestamps[-1]:
+        num_current_bin = inhale_ts.between(win_start, win_end).sum()
+        frequency = _calc_freq(num_current_bin, window_size_ms)
+        post_fv_ts_bins.loc[win_end, 'count'] = num_current_bin
+        post_fv_ts_bins.loc[win_end, 'frequency'] = frequency
+        win_start += window_hop_ms
+        win_end = win_start + window_size_ms
+
+    print(pre_fv_ts_bins, post_fv_ts_bins)
