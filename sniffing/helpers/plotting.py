c56d49fc3d8d27670b3dbed8afb07da2b0064369
Austin Pauley <austin2118ace@gmail.com>
2025-05-21 12:06:03 -0400

    add trial name to raw plots; new plot function for binned frequencies

--- sniffing/helpers/plotting.py
+++ sniffing/helpers/plotting.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pandas import Series
 
-def plot_multi_traces(traces: list[Series]) -> None:
+def plot_multi_traces(traces: list[Series], trial_name) -> None:
     """
     Plots multiple traces in individual rows. Useful for visualizing raw v. preprocessed traces
     :param traces: (list[Series]) List of Pandas Series. Each series is a trace that is plotted on an individual subplot
@@ -15,6 +15,7 @@ def plot_multi_traces(traces: list[Series]) -> None:
         _ = axs[i].plot(trace, color=color)
         _ = axs[i].vlines(x=0, ymin=min(trace), ymax=max(trace), color='k')
         _ = axs[i].hlines(y=np.mean(trace), xmin=trace.index[0], xmax=trace.index[-1], color='m')
+    plt.suptitle(trial_name)
 
     plt.show()
 
@@ -33,8 +34,10 @@ def plot_crossing_frequencies(trace, true_inhales, true_exhales, inhale_frequenc
     _ = ax[0].hlines(y=np.mean(trace), xmin=trace.index[0],
                      xmax=trace.index[-1], alpha=0.3, color='m')
     _ = ax[0].scatter(crossings, np.zeros(len(crossings)), marker="o", color='g')
-    _ = ax[0].scatter(true_inhales.index, true_inhales['magnitude'].values, marker='x', color='r', label='inhale')
-    _ = ax[0].scatter(true_exhales.index, true_exhales['magnitude'].values, marker='x', color='orange', label='exhale')
+    # _ = ax[0].scatter(true_inhales.index, true_inhales['magnitude'].values, marker='x', color='r', label='inhale')
+    # _ = ax[0].scatter(true_exhales.index, true_exhales['magnitude'].values, marker='x', color='orange', label='exhale')
+    _ = ax[0].scatter(true_inhales, true_inhales.index, marker='x', color='r', label='inhale')
+    _ = ax[0].scatter(true_exhales, true_exhales.index, marker='x', color='orange', label='exhale')
     _ = ax[0].legend()
 
     _ = ax[1].legend(['inhale', 'exhale'])
@@ -45,3 +48,33 @@ def plot_crossing_frequencies(trace, true_inhales, true_exhales, inhale_frequenc
         plt.close()
     else:
         plt.show()
+
+
+def plot_binned_frequencies(traces, titles, num_trials, mouse, concentration):
+    colors = ['m', 'c', 'g', 'orange']
+    num_traces = len(traces)
+    fig,ax = plt.subplots(num_traces, 1, figsize=(10, 10), sharex=True, sharey=True)
+
+    max_y = [max(trace) for trace in traces if len(trace) > 0]
+    max_y = max(max_y) * 1.1
+
+    for i, trace in enumerate(traces):
+        ax[i].plot(trace, color=colors[i])
+        ax[i].set_title(f'{titles[i]} - Num Trials: {num_trials[i]}')
+        ax[i].set_ylim(0, max_y)
+    plt.suptitle(f'Mouse: {mouse} - Concentration: {concentration}')
+    fig.supxlabel('Time (ms, relative to odor onset)')
+    fig.supylabel('Frequency (Hz)')
+
+    # for i, trace in enumerate(traces):
+    #     ax[i].bar(trace.index.values, trace.values, width=1.5, color=colors[i])
+    #     ax[i].set_title(f'{titles[i]} - Num Trials: {num_trials[i]}')
+    #     # ax[i].set_ylim(0, max_y)
+    # plt.suptitle(f'Mouse: {mouse} - Concentration: {concentration}')
+    # fig.supxlabel('Time (ms, relative to odor onset)')
+    # fig.supylabel('Frequency (Hz)')
+
+    plt.tight_layout()
+    plt.show()
+
+    return fig, ax
