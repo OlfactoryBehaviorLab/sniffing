b2c2744ab9a717ca5612e5c6e7d27c6902443f5d
Austin Pauley <austin2118ace@gmail.com>
2025-05-21 12:09:59 -0400

    one-sided sliding window frequency counts using numba; changed two-sided end detection criteria

--- sniffing/helpers/frequency.py
+++ sniffing/helpers/frequency.py
@@ -3,6 +3,8 @@ import pandas as pd
 from scipy import signal, fft
 import matplotlib.pyplot as plt
 
+from numba import njit
+from numba import types, typed
 def _instantaneous_frequency(peaks):
     pairs = zip(peaks.iloc[:-1].index, peaks.iloc[1:].index)
     frequencies = []
@@ -23,7 +25,7 @@ def calc_frequencies(true_inhales, true_exhales):
 
     return inhale_frequencies, exhale_frequencies, inhale_times, exhale_times
 
-
+@njit
 def _calc_freq(counts, bin_dur_ms):
     if counts == 0:
         return 0
@@ -31,7 +33,7 @@ def _calc_freq(counts, bin_dur_ms):
         return counts / (bin_dur_ms / 1000)
 
 
-def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, window_size_ms: int=250, window_hop_ms: int=10):
+def twoside_moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, window_size_ms: int=100, window_hop_ms: int=10):
     """
 
     :param inhale_ts:
@@ -47,8 +49,7 @@ def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, w
 
     win_end = pre_fv_timestamps[-1]
     win_start = win_end - window_size_ms
-
-    while win_end >= pre_fv_timestamps[0] + window_size_ms:
+    while win_start >= pre_fv_timestamps[0]:
         num_current_bin = inhale_ts.between(win_start, win_end).sum()
         frequency = _calc_freq(num_current_bin, window_size_ms)
         ts_bins.loc[win_end, 'count'] = num_current_bin
@@ -58,7 +59,7 @@ def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, w
 
     win_start = post_fv_timestamps[0]
     win_end = win_start + window_size_ms
-    while win_end <= (post_fv_timestamps[-1] - window_size_ms):
+    while win_end <= (post_fv_timestamps[-1]):
         num_current_bin = inhale_ts.between(win_start, win_end).sum()
         frequency = _calc_freq(num_current_bin, window_size_ms)
         ts_bins.loc[win_start, 'count'] = num_current_bin
@@ -67,3 +68,31 @@ def moving_window_frequency(inhale_ts: pd.Series, trial_timestamps: pd.Series, w
         win_end = win_start + window_size_ms
 
     return ts_bins.sort_index()
+
+@njit
+def oneside_moving_window_frequency(inhale_ts: np.array, trial_timestamps: np.array, window_size_ms: int=100, window_step_ms: int=10):
+    """
+
+    :param inhale_ts:
+    :param trial_timestamps:
+    :param window_size_ms:
+    :param window_step_ms:
+    :return:
+    """
+
+    start_bins = np.arange(trial_timestamps[0], trial_timestamps[-1], window_step_ms)
+    end_bins = start_bins + window_size_ms
+    good_bin_indices = np.where(end_bins <= trial_timestamps[-1])[0]
+    window_bins = np.array(list(zip(start_bins, end_bins)))[good_bin_indices]
+    bin_centers = np.zeros(window_bins.shape[0])
+    counts = np.zeros(window_bins.shape[0])
+    frequencies = np.zeros(window_bins.shape[0])
+
+    for i, (win_start, win_end) in enumerate(window_bins):
+        num_sniffs = np.logical_and(win_end > inhale_ts, inhale_ts >= win_start).sum()
+        frequency = _calc_freq(num_sniffs, window_size_ms)
+        bin_centers[i] = (win_start + win_end) / 2
+        counts[i] = num_sniffs
+        frequencies[i] = frequency
+
+    return bin_centers, counts, frequencies
