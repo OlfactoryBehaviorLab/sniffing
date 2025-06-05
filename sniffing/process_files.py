b05b5ee8d0cd78f717ace86cb0238e2dccc9db4a
Austin Pauley <austin2118ace@gmail.com>
2025-06-05 11:26:00 -0400

    format process_files

--- sniffing/process_files.py
+++ sniffing/process_files.py
@@ -18,57 +18,105 @@ PRE_ODOR_COUNT_TIME_MS = -350
 POST_ODOR_COUNT_TIME_MS = 350
 
 
-def process_files(h5_files, output_dir, run_manual_curation=False, filtered_manual_curation=True,
-                  plot_figs=False, display_plots=False, ignore_errors=False):
-    for h5_file_path in tqdm(h5_files, total=len(h5_files), desc='Processing H5 Files:'):
+def process_files(
+    h5_files,
+    output_dir,
+    run_manual_curation=False,
+    filtered_manual_curation=True,
+    plot_figs=False,
+    display_plots=False,
+    ignore_errors=False,
+):
+    for h5_file_path in tqdm(
+        h5_files, total=len(h5_files), desc="Processing H5 Files:"
+    ):
         try:
-            print(f'Processing {h5_file_path.name}')
+            print(f"Processing {h5_file_path.name}")
 
             with DewanH5(h5_file_path, drop_early_lick_trials=True) as h5:
-                inhale_counts = pd.DataFrame(index=[PRE_ODOR_COUNT_TIME_MS, POST_ODOR_COUNT_TIME_MS])
+                inhale_counts = pd.DataFrame(
+                    index=[PRE_ODOR_COUNT_TIME_MS, POST_ODOR_COUNT_TIME_MS]
+                )
                 inhale_latencies = pd.DataFrame(index=[1, 2, 3])
                 inhale_durations = pd.DataFrame()
 
-                file_output_dir = output_dir.joinpath(f'mouse-{h5.mouse}', h5.concentration)
+                file_output_dir = output_dir.joinpath(
+                    f"mouse-{h5.mouse}", h5.concentration
+                )
                 file_output_dir.mkdir(exist_ok=True, parents=True)
 
-                bp_filter = signal.cheby2(2, 40, [LOWER_FILTER_BAND, UPPER_FILTER_BAND], 'bandpass',
-                                          output='sos', fs=1000)
-                filtered_traces = preprocessing.filter_sniff_traces(h5.sniff, bp_filter, baseline=True, z_score=True)
+                bp_filter = signal.cheby2(
+                    2,
+                    40,
+                    [LOWER_FILTER_BAND, UPPER_FILTER_BAND],
+                    "bandpass",
+                    output="sos",
+                    fs=1000,
+                )
+                filtered_traces = preprocessing.filter_sniff_traces(
+                    h5.sniff, bp_filter, baseline=True, z_score=True
+                )
 
                 filtered_trace_keys = list(filtered_traces.keys())
 
                 if run_manual_curation:
                     if filtered_manual_curation:
-                        gui_sniff_traces = analog_trace.AnalogTrace.generate_sniff_traces(filtered_trace_keys, h5,
-                                                                                          filtered_traces=filtered_traces)
+                        gui_sniff_traces = (
+                            analog_trace.AnalogTrace.generate_sniff_traces(
+                                filtered_trace_keys, h5, filtered_traces=filtered_traces
+                            )
+                        )
                     else:
-                        gui_sniff_traces = analog_trace.AnalogTrace.generate_sniff_traces(filtered_trace_keys, h5)
+                        gui_sniff_traces = (
+                            analog_trace.AnalogTrace.generate_sniff_traces(
+                                filtered_trace_keys, h5
+                            )
+                        )
 
-                    filtered_trace_keys = manual_curation.launch_sniff_gui(gui_sniff_traces, filtered_trace_keys)
+                    filtered_trace_keys = manual_curation.launch_sniff_gui(
+                        gui_sniff_traces, filtered_trace_keys
+                    )
 
-                for trial_number in tqdm(filtered_trace_keys, total=len(filtered_trace_keys), leave=True, position=0):
-                    filtered_trimmed_trace = filtered_traces[trial_number].loc[PRE_FV_TIME:]
+                for trial_number in tqdm(
+                    filtered_trace_keys,
+                    total=len(filtered_trace_keys),
+                    leave=True,
+                    position=0,
+                ):
+                    filtered_trimmed_trace = filtered_traces[trial_number].loc[
+                        PRE_FV_TIME:
+                    ]
                     raw_data = h5.sniff[trial_number].loc[PRE_FV_TIME:]
 
                     # Find inhales/exhales and select true inhales
-                    inhales, exhales, crossings = preprocessing.get_trace_features(filtered_trimmed_trace)
+                    inhales, exhales, crossings = preprocessing.get_trace_features(
+                        filtered_trimmed_trace
+                    )
                     true_inhales = preprocessing.filter_sniff_peaks(inhales, exhales)
 
                     true_inhales_post_fv = true_inhales.loc[0:]
                     if len(true_inhales_post_fv) == 0:
-                        print(f'{trial_number} has no inhales after the FV!')
+                        print(f"{trial_number} has no inhales after the FV!")
                         continue
 
                     # Gather the flanking exhale for each inhale and find the inhalation durations
-                    flanking_exhales = preprocessing.get_flanking_exhales(true_inhales.index, exhales)
-                    trial_inhalation_duration = preprocessing.get_inhalation_durations(true_inhales, flanking_exhales,
-                                                                                       filtered_trimmed_trace)
-                    trial_inhalation_duration = trial_inhalation_duration.reset_index(names='timestamps')
+                    flanking_exhales = preprocessing.get_flanking_exhales(
+                        true_inhales.index, exhales
+                    )
+                    trial_inhalation_duration = preprocessing.get_inhalation_durations(
+                        true_inhales, flanking_exhales, filtered_trimmed_trace
+                    )
+                    trial_inhalation_duration = trial_inhalation_duration.reset_index(
+                        names="timestamps"
+                    )
                     new_columns = pd.MultiIndex.from_product(
-                        [[str(trial_number)], trial_inhalation_duration.columns.values], names=['Trial', 'Data'])
+                        [[str(trial_number)], trial_inhalation_duration.columns.values],
+                        names=["Trial", "Data"],
+                    )
                     trial_inhalation_duration.columns = new_columns
-                    trial_inhalation_duration = trial_inhalation_duration.infer_objects().fillna('X')
+                    trial_inhalation_duration = (
+                        trial_inhalation_duration.infer_objects().fillna("X")
+                    )
                     inhale_durations = pd.concat(
                         (inhale_durations, trial_inhalation_duration), axis=1
                     )
@@ -77,21 +125,35 @@ def process_files(h5_files, output_dir, run_manual_curation=False, filtered_manu
                     post_odor_sniffs = true_inhales.loc[0:POST_ODOR_COUNT_TIME_MS]
                     pre_odor_sniff_count = pre_odor_sniffs.shape[0]
                     post_odor_sniff_count = post_odor_sniffs.shape[0]
-                    _counts = pd.Series((pre_odor_sniff_count, post_odor_sniff_count), name=trial_number)
+                    _counts = pd.Series(
+                        (pre_odor_sniff_count, post_odor_sniff_count), name=trial_number
+                    )
 
                     inhale_counts = pd.concat((inhale_counts, _counts), axis=1)
 
-                    post_odor_latencies = pd.Series(np.zeros(3), index=[1, 2, 3], name=trial_number)
+                    post_odor_latencies = pd.Series(
+                        np.zeros(3), index=[1, 2, 3], name=trial_number
+                    )
                     _latencies = post_odor_sniffs.index[:3]
-                    post_odor_latencies.loc[:_latencies.shape[0]] = _latencies
+                    post_odor_latencies.loc[: _latencies.shape[0]] = _latencies
 
-                    inhale_latencies = pd.concat((inhale_latencies, post_odor_latencies), axis=1)
+                    inhale_latencies = pd.concat(
+                        (inhale_latencies, post_odor_latencies), axis=1
+                    )
 
-                    plot_output_dir = file_output_dir.joinpath('figures')
+                    plot_output_dir = file_output_dir.joinpath("figures")
                     plot_output_dir.mkdir(exist_ok=True, parents=True)
                     if plot_figs:
-                        plotting.plot_traces(raw_data, filtered_trimmed_trace, true_inhales, inhales, exhales,
-                                             trial_number, plot_output_dir, display_plots)
+                        plotting.plot_traces(
+                            raw_data,
+                            filtered_trimmed_trace,
+                            true_inhales,
+                            inhales,
+                            exhales,
+                            trial_number,
+                            plot_output_dir,
+                            display_plots,
+                        )
 
                 # go_trial_counts.to_excel(file_output_dir.joinpath('go_trial_counts.xlsx'))
                 # false_alarm_counts.to_excel(file_output_dir.joinpath('false_alarm_counts.xlsx'))
@@ -101,13 +163,19 @@ def process_files(h5_files, output_dir, run_manual_curation=False, filtered_manu
                 # fig.savefig(file_output_dir.joinpath(f'binned_frequency_hist.pdf'))
                 output
 
-                output.repack_data(h5, inhale_counts, inhale_latencies, inhale_durations, file_output_dir)
+                output.repack_data(
+                    h5,
+                    inhale_counts,
+                    inhale_latencies,
+                    inhale_durations,
+                    file_output_dir,
+                )
                 h5.export(file_output_dir)
-
 
         except Exception as e:
             import traceback
-            print(f'Error processing H5 file {h5_file_path}')
+
+            print(f"Error processing H5 file {h5_file_path}")
             print(traceback.format_exc())
             if not ignore_errors:
                 raise e
