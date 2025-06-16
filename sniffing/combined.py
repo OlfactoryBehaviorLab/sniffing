45973eadfb7c7a42fdaf094e6ec7f60c7db7b742
Austin Pauley <austin2118ace@gmail.com>
2025-06-16 12:57:07 -0400

    make sure that we only use the trials we have windows for, save output to a log file

--- sniffing/combined.py
+++ sniffing/combined.py
@@ -1,3 +1,4 @@
+import logging
 from functools import reduce
 from warnings import warn
 import pandas as pd
@@ -6,6 +7,8 @@ import numpy as np
 from tqdm.auto import tqdm
 
 from .process_files import PRE_ODOR_COUNT_TIME_MS, POST_ODOR_COUNT_TIME_MS
+
+logging.basicConfig(level=logging.NOTSET, filename='trial_nums.log', encoding='utf-8')
 
 def animals_to_skip(concentration_files: dict[str, dict]) -> tuple[list, list]:
     all_keys = [list(concentration_files[concentration].keys()) for concentration in concentration_files]
@@ -28,13 +31,19 @@ def process_combined(concentration_files: dict[str, dict]):
 
         animal_files = concentration_files[concentration]
 
+
         for animal in animal_files:
             animal_data_matrix = pd.read_excel(animal_files[animal]['combined'], index_col=[0])
             windowed_bin_counts = pd.read_excel(animal_files[animal]['window'], index_col=[0])
 
+            good_trials = windowed_bin_counts.columns
+
             trial_types = animal_data_matrix['trial_type']
             go_trials = animal_data_matrix.loc[trial_types == 1].index
             nogo_trials = animal_data_matrix.loc[trial_types == 2].index
+
+            go_trials = np.intersect1d(good_trials, go_trials)
+            nogo_trials = np.intersect1d(good_trials, nogo_trials)
 
             go_trial_counts = windowed_bin_counts.loc[PRE_ODOR_COUNT_TIME_MS:POST_ODOR_COUNT_TIME_MS, go_trials]
             nogo_trial_counts = windowed_bin_counts.loc[PRE_ODOR_COUNT_TIME_MS:POST_ODOR_COUNT_TIME_MS, nogo_trials]
@@ -47,4 +56,4 @@ def process_combined(concentration_files: dict[str, dict]):
         all_go_trials.columns = np.repeat('go', all_go_trials.shape[1])
         all_nogo_trials.columns = np.repeat('nogo', all_nogo_trials.shape[1])
 
-        break
+        logging.info("Concentration %s has %i go trials and %i nogo trials", concentration, all_go_trials.shape[1], all_nogo_trials.shape[1])
