d6c770aa2641be0bfa2a45c96d5359aaf4bcb40c
Austin Pauley <austin2118ace@gmail.com>
2025-06-16 18:53:09 -0400

    get means, variances, and scores from each concentration using a for loop

--- sniffing/combined.py
+++ sniffing/combined.py
@@ -8,6 +8,8 @@ from tqdm.auto import tqdm
 
 from .process_files import PRE_ODOR_COUNT_TIME_MS, POST_ODOR_COUNT_TIME_MS
 
+from .helpers import classifiers
+
 logging.basicConfig(level=logging.NOTSET, filename='trial_nums.log', encoding='utf-8')
 
 def animals_to_skip(concentration_files: dict[str, dict]) -> tuple[list, list]:
@@ -19,10 +21,12 @@ def animals_to_skip(concentration_files: dict[str, dict]) -> tuple[list, list]:
     return good_animals, bad_animals
 
 
-def process_combined(concentration_files: dict[str, dict]):
+def process_combined(concentration_files: dict[str, dict], output_dir):
 
     good_animals, bad_animals = animals_to_skip(concentration_files)
     warn(f'{bad_animals} are missing some concentrations and will be skipped!', stacklevel=2)
+
+    concentration_dfs = {}
 
     for concentration in tqdm(concentration_files, desc="Processing concentrations: ", total=len(concentration_files),
                               leave=True, position=1):
@@ -56,4 +60,30 @@ def process_combined(concentration_files: dict[str, dict]):
         all_go_trials.columns = np.repeat('go', all_go_trials.shape[1])
         all_nogo_trials.columns = np.repeat('nogo', all_nogo_trials.shape[1])
 
+        all_concentration_trials = pd.concat([all_go_trials, all_nogo_trials], axis=1)
+        concentration_dfs[concentration] = all_concentration_trials.T
         logging.info("Concentration %s has %i go trials and %i nogo trials", concentration, all_go_trials.shape[1], all_nogo_trials.shape[1])
+
+
+    all_concentration_labels = list(concentration_dfs.keys())
+    # scores, individual_scores, individual_CMS = classifiers.decode_trial_type(concentration_dfs[all_concentration_labels[0]])
+    _cols = concentration_dfs[all_concentration_labels[0]].columns
+    all_scores = pd.DataFrame(index=all_concentration_labels, columns=_cols)
+    all_variance = pd.DataFrame(index=all_concentration_labels, columns=_cols)
+    all_means = pd.DataFrame(index=all_concentration_labels, columns=_cols)
+
+    for concentration in all_concentration_labels:
+        concentration_df = concentration_dfs[concentration]
+        all_variance.loc[concentration] = concentration_df.var()
+        all_means.loc[concentration] = concentration_df.mean()
+
+        scores, individual_scores, individual_CMS = classifiers.decode_trial_type_single(concentration_df)
+        all_scores.loc[concentration] = scores
+
+    all_scores_path = output_dir.joinpath('all_scores.xlsx')
+    all_variance_path = output_dir.joinpath('all_variance.xlsx')
+    all_means_path = output_dir.joinpath('all_means.xlsx')
+
+    all_scores.to_excel(all_scores_path)
+    all_variance.to_excel(all_variance_path)
+    all_means.to_excel(all_means_path)
